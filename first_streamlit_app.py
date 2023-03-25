620f3c3ecb1d785a2f3914dcefc0db1f58771a75
snowflakefox <128926249+snowflakefox@users.noreply.github.com>
2023-03-25 19:31:24 +0000

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -6,4 +6,6 @@ streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑 Avocado and 🍞Toast')
 
+🥗

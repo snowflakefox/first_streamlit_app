a54d814f8289d8476864c081140d49ec84159d7b
snowflakefox <128926249+snowflakefox@users.noreply.github.com>
2023-04-02 17:21:54 +0100

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -43,3 +43,7 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
+# Let's put a pick list here so they can pick the fruit they want to include 
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc [fruits_selected]
+# Display the table on the page.

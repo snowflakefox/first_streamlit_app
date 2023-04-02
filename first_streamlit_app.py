fc8d16482de14b72266e80bc51870649bdc6b674
snowflakefox <128926249+snowflakefox@users.noreply.github.com>
2023-04-02 18:55:00 +0100

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -54,7 +54,7 @@ try:
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
-  streamlit.stop()
+streamlit.stop()
   
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
 streamlit.write('The user entered ', add_my_fruit)

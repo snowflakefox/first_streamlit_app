02bfadc3970080e991eb4d060ac04a5c136b5cb0
snowflakefox <128926249+snowflakefox@users.noreply.github.com>
2023-04-02 18:25:31 +0100

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -41,7 +41,7 @@ try:
       back_from_function = get_fruityvice_data(fruit_choice)
       streamlit.dataframe(back_from_function)
                      
-
+#dont run anyhting past here
 streamlit.stop()
 
 #import snowflake.connector
